ac02ecb7ea991f26f1d7c5b25c76c7c7e48d228c
Jiwon Jeong <jiwon.j.jeong@gmail.com>
2024-05-14 14:36:42 -0700

    Write pseudocode for getComputerChoice function
    Write pseudocode to test and get a random string for rock, paper, or scissors.

--- scripts.js
+++ scripts.js
@@ -1,1 +1,16 @@
 console.log("Hello World");
+
+// function getComputerChoice will randomly return "rock", "paper", or "scissors" string
+// INIT function getComputerChoice
+    // INIT number randomInteger
+    // SET randomInteger to 0, 1, or 2 randomly using Math.random
+    // IF randomInteger is 0 THEN
+        // RETURN "rock"
+    // ELSE IF randomInteger is 0 THEN
+        // RETURN "paper"
+    // ELSE
+        // RETURN "scissors"
+    // END IF
+// END function
+
+// PRINT function twice to test getComputerChoice
