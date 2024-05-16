6bc5cbe3750de9d7da8a65b0e0b5e76854df8821
Jiwon Jeong <jiwon.j.jeong@gmail.com>
2024-05-16 14:31:56 -0700

    Display and update scores after rounds
    Add display to page and update text using DOM manipulation after each round.

--- scripts.js
+++ scripts.js
@@ -23,11 +23,13 @@ console.log(getComputerChoice());
 
 // function playRound will simulate a single round
 // function will take two string inputs for human then computer, then compare them to determine winner.
-// function will then output a message declaring how the round went
-// function increments appropriate score variable based on who won
+// function will then output a result message by appending new paragraph onto "results" div
+// function increments appropriate score variable based on who won and updates displayed scores
 function playRound(humanChoice, computerChoice){
     const roundResult = document.createElement("p");
     const resultTable = document.querySelector(".results");
+    const humanScoreText = document.querySelector(".humanScore");
+    const computerScoreText = document.querySelector(".computerScore");
     if (humanChoice === "rock"){
         if (computerChoice === "rock"){
             roundResult.textContent="You tie! We both chose rock.";
@@ -62,6 +64,8 @@ function playRound(humanChoice, computerChoice){
         roundResult.textContent= "You didn't choose 'rock', 'paper', or 'scissors'!";
     }
     resultTable.appendChild(roundResult);
+    humanScoreText.textContent = "Your score: " + humanScore;
+    computerScoreText.textContent = "My score: " + computerScore;
 }
 
 
@@ -83,6 +87,7 @@ function playGame(){
     }
 }
 
+// display score
 
 // add event listeners to 3 buttons for rock, paper, or scissors choice
 const rockButton = document.querySelector(".rock");
